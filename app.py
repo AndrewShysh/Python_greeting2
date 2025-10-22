eec633daf69ed2fd9026ea614d70ac14f1b6a691
AndrewShysh <93144869+AndrewShysh@users.noreply.github.com>
2025-10-22 11:35:25 +0300

    typo correct

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def greet(name):
-    return f"Hello, {name}! The currecnt time is {datetime.now():%H:%M:%S}"
+    return f"Hello, {name}! The current time is {datetime.now():%H:%M:%S}"
 
 if __name__ == "__main__":
     name = input("enter your name:")
