9bc55eb06a0a5a2e344fc7d303aa01895a172116
AndrewShysh <93144869+AndrewShysh@users.noreply.github.com>
2025-10-22 10:56:07 +0300

    Initial commit: simple greeting app

---
+++ app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def greet(name):
+    return f"Hello, {name}! The currecnt time is {datetime.now():%H:%M:%S}"
+
+if __name__ == "__main__":
+    print(greet('world'))
+
